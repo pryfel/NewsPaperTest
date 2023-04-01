b8e38ba5a10e6af63f4e9ea831e867b7777634ca
pryfel <pryfel@gmail.com>
2023-04-01 17:47:06 +0300

    Updated models

--- mc_donalds/models.py
+++ mc_donalds/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 director = 'DI'
@@ -15,8 +16,6 @@ POSITIONS = [
     (cleaner, 'Уборщик')
 ]
 
-class Order(models.Model):
-    pass
 
 # CREATE TABLE PRODUCTS(
 # 	product_id INT AUTO_INCREMENT NOT NULL,
@@ -38,6 +37,36 @@ class Staff(models.Model):
                                 default=cashier)
     labor_contract = models.IntegerField()
 
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:
+            return (datetime.now() - self.time_in).total_seconds() // 60
+
 
 class ProductOrder(models.Model):
-    pass
+    amount = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
